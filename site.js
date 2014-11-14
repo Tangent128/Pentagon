bc142edf71a1863aa530ea6e9e57a170629ec7ff
Tangent 128 <Tangent128@gmail.com>
2014-11-14 12:37:09 -0500

    * HTML DomParser polyfill * start on page-processing logic

--- site.js
+++ site.js
@@ -7,20 +7,42 @@ var PENTAGON_THEME_URL = "site.html";
 /* Theme Applicator Script */
 Pentagon.push(function($, loaded) {
 	
+	var htmlParser = loaded("htmlParser");
+	
+	/* Keep track of pages' nav links for swapout, while
+	 * importing scripts/styles sitewide */
+	var loadedFiles = {};
+	var $head = $("head");
+	var $body = $("body");
+	
+	function processPage($page) {
+		function getLinks(rel) {
+			var $links = $page.find("head link[rel="+rel+"]");
+			if($links.length == 0) {
+				$links = $page.find("body a[rel="+rel+"]");
+			}
+			return $links;
+		}
+		
+		var record = {};
+		record.url = getLinks("self").attr("href");
+		record.div = $("<div>").append($page.find("body").content());
+		
+		return record;
+	}
+	
 	/* Fetch & apply theme */
-	$.ajax({
+	var themeRequest = $.ajax({
 		url: PENTAGON_THEME_URL,
 		dataType: "html"
-	}).then(function(html) {
-		var $body = $("body");
+	});
+	
+	$.when(themeRequest, htmlParser).then(function(themeResult, parser) {
+		var html = themeResult[0];
 		// parse template
-		// jQuery loses track of html/head/body tags in parsing,
-		// so we hackily find those parts of the page on our own. >.<
-		var head = html.match(/<head>([\s\S]*)<\/head>/m);
-		var body = html.match(/<body>([\s\S]*)<\/body>/m);
-		
-		var $themeHead = $("<div>").append($.parseHTML(head[1]));
-		var $themeBody = $.parseHTML(body[1]);
+		var $theme = $(parser.parseFromString(html, "text/html"));
+		var $themeHead = $theme.find("head");
+		var $themeBody = $theme.find("body").contents();
 		
 		// grab template & page content
 		var $initialContent = $body.contents();
@@ -64,3 +86,30 @@ Pentagon.push(function($, loaded) {
 	document.head.appendChild(script);
 	
 })();
+
+/* HTML Parser hacky polyfill */
+Pentagon.push(function($, loaded) {
+	try {
+		// inspired by https://gist.github.com/eligrey/1129031
+		var parser = new DOMParser();
+		if(parser.parseFromString("", "text/html")) {
+			loaded("htmlParser").resolve(parser);
+			return;
+		}
+	} catch(ex) {}
+	
+	loaded("htmlParser").resolve({
+		parseFromString: function(src) {
+			var html = $("<html>");
+			
+			// jQuery loses track of html/head/body tags in parsing,
+			// so we hackily find those parts of the page on our own. >.<
+			var head = $("<head>").html(src.match(/<head>([\s\S]*)<\/head>/m)[1]);
+			var body = $("<body>").html(src.match(/<body>([\s\S]*)<\/body>/m)[1]);
+			
+			html.append(head);
+			html.append(body);
+			return html;
+		}
+	});
+});
